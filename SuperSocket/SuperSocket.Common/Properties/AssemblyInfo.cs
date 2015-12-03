d73f301d1a00eb61b181d60646e1ba01cff42ad5
luchunminglu <luchunminglu@gmail.com>
2015-12-03 22:32:37 +0800

    cc

--- SuperSocket/SuperSocket.Common/Properties/AssemblyInfo.cs
+++ SuperSocket/SuperSocket.Common/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+//定义该程序集为cls兼容的
+[assembly: System.CLSCompliant(true)]
