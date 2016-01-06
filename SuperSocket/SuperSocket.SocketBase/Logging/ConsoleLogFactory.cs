028359d10135976663ae015860f70e1adbda5a3c
luchunminglu <luchunminglu@gmail.com>
2016-01-06 09:33:56 +0800

    cc

---
+++ SuperSocket/SuperSocket.SocketBase/Logging/ConsoleLogFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.SocketBase.Logging
+{
+    /// <summary>
+    /// Console log factory
+    /// </summary>
+    public class ConsoleLogFactory:ILogFactory
+    {
+        /// <summary>
+        /// Get the log by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public ILog GetLog(string name)
+        {
+            return new ConsoleLog(name);
+        }
+    }
+}
---
+++ SuperSocket/SuperSocket.SocketBase/Logging/Log4NetLog.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.SocketBase.Logging
+{
+    public class Log4NetLog:ILog
+    {
+        #region Member & Constructor
+
+        private log4net.ILog m_Log;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="log"></param>
+        public Log4NetLog(log4net.ILog log)
+        {
+            if (log == null)
+            {
+                throw new ArgumentNullException("log");
+            }
+
+            m_Log = log;
+        }
+
+        #endregion
+
+        #region Ilog  接口
+
+        public bool IsDebugEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsErrorEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsFatalEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsInfoEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsWarnEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Debug(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
