d6dfc6798abe21e3c3b12628fcd019302f5181a9
luchunminglu <luchunminglu@gmail.com>
2016-01-06 09:08:31 +0800

    cc

---
+++ SuperSocket/SuperSocket.SocketBase/Logging/ConsoleLog.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.SocketBase.Logging
+{
+    public class ConsoleLog:ILog
+    {
+        #region Private Member
+
+        private string m_Name;
+
+        private const string m_MessageTemplate = "{0}-{1}:{2}";
+
+        private const string m_Debug = "DEBUG";
+
+        private const string m_Error = "ERROR";
+
+        private const string m_Fatal = "FATAL";
+
+        private const string m_Info = "INFO";
+
+        private const string m_Warn = "WARN";
+
+        #endregion
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name"></param>
+        public ConsoleLog(string name)
+        {
+            this.m_Name = name;
+        }
+
+        #region ILog 接口
+
+        /// <summary>
+        /// Get a value indicating whether this instance is debug enabled
+        /// </summary>
+        public bool IsDebugEnabled
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Get a value indicating whether this instance is error enabled
+        /// </summary>
+        public bool IsErrorEnabled
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Get a value indicating whether this instance is fatal enabled
+        /// </summary>
+        public bool IsFatalEnabled
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is info enabled
+        /// </summary>
+        public bool IsInfoEnabled
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Get a value indicating whether this instance is warn enabled
+        /// </summary>
+        public bool IsWarnEnabled
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Logs the debug message.
+        /// </summary>
+        /// <param name="message"></param>
+        public void Debug(object message)
+        {
+            Console.WriteLine(m_MessageTemplate,m_Name,m_Debug,message);
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FatalFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(IFormatProvider provider, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0, object arg1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarnFormat(string format, object arg0, object arg1, object arg2)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ SuperSocket/SuperSocket.SocketBase/ServerState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.SocketBase
+{
+    public enum ServerState:int
+    {
+        /// <summary>
+        /// Not initialized
+        /// </summary>
+        NotInitialized = ServerStateConst.NotInitialized,
+
+        /// <summary>
+        /// In initializing
+        /// </summary>
+        Initializing  = ServerStateConst.Initializing,
+
+        /// <summary>
+        /// Has been initialized, but not started
+        /// </summary>
+        NotStarted = ServerStateConst.NotStarted,
+
+        /// <summary>
+        /// In starting
+        /// </summary>
+        Starting = ServerStateConst.Starting,
+
+        /// <summary>
+        /// In running
+        /// </summary>
+        Running = ServerStateConst.Running,
+
+        /// <summary>
+        /// In stopping
+        /// </summary>
+        Stopping = ServerStateConst.Stopping,
+    }
+
+    /// <summary>
+    /// Server state 常量
+    /// </summary>
+    internal class ServerStateConst
+    {
+        public const int NotInitialized = 0;
+
+        public const int Initializing = 1;
+
+        public const int NotStarted = 2;
+
+        public const int Starting = 3;
+
+        public const int Running = 4;
+
+        public const int Stopping = 5;
+    }
+}
